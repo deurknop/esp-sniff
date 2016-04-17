e233d352ff37bf31d963c663a3bd75edc9d2656c
deurknop <deurknop64@gmail.com>
2016-04-17 17:51:15 +0200

    Show MACs in serial monitor (heavy)

--- esp-sniff.ino
+++ esp-sniff.ino
@@ -2,9 +2,114 @@ extern "C" {
 #include "user_interface.h"
 }
 
+#define FRAME_TYPE_MANAGEMENT 0
+#define FRAME_TYPE_CONTROL 1
+#define FRAME_TYPE_DATA 2
+#define FRAME_SUBTYPE_PROBE_REQUEST 0x04
+#define FRAME_SUBTYPE_PROBE_RESPONSE 0x05
+#define FRAME_SUBTYPE_BEACON 0x08
+#define FRAME_SUBTYPE_AUTH 0x0b
+#define FRAME_SUBTYPE_DEAUTH 0x0c
+#define FRAME_SUBTYPE_DATA 0x14
+typedef struct framectrl_80211
+{
+    //buf[0]
+    u8 Protocol:2;
+    u8 Type:2;
+    u8 Subtype:4;
+    //buf[1]
+    u8 ToDS:1;
+    u8 FromDS:1;
+    u8 MoreFlag:1;
+    u8 Retry:1;
+    u8 PwrMgmt:1;
+    u8 MoreData:1;
+    u8 Protectedframe:1;
+    u8 Order:1;
+} framectrl_80211,*lpframectrl_80211;
+
+typedef struct w80211_header
+{
+  struct framectrl_80211 framectrl;
+  uint16 duration;
+  uint8 rdaddr[6]; // receiver
+  uint8 tsaddr[6]; // transmitter
+  uint8 bssid[6];
+  uint16 number; // sequence number
+} w80211header;
+
+typedef struct tagged_parameter
+{
+  /* SSID parameter */
+  uint8 tag_number;
+  uint8 tag_length;
+} tagged_parameter, *ptagged_parameter;
+
+struct RxControl {
+    signed rssi:8;// indicates the signal strength of the package
+    unsigned rate:4;
+    unsigned is_group:1;
+    unsigned:1;
+    unsigned sig_mode:2;// indicates whether the packet is a packet 11n, 0 represents a non-11n, 11n nonzero
+    unsigned legacy_length:12;// if not 11n package, it represents the length of the package
+    unsigned damatch0:1;
+    unsigned damatch1:1;
+    unsigned bssidmatch0:1;
+    unsigned bssidmatch1:1;
+    unsigned MCS:7;// if the package is 11n, which represents the modulation coding sequence packets RMS: 0-76
+    unsigned CWB:1;//  if the package is 11n, which indicates whether the packet HT40
+    unsigned HT_length:16;// if the package is 11n, which represents the length of the package
+    unsigned Smoothing:1;
+    unsigned Not_Sounding:1;
+    unsigned:1;
+    unsigned Aggregation:1;
+    unsigned STBC:2;
+    unsigned FEC_CODING:1;// if the package is 11n, which indicates whether the packet LDPC
+    unsigned SGI:1;
+    unsigned rxend_state:8;
+    unsigned ampdu_cnt:8;
+    unsigned channel:4;// indicates where the packet channel
+    unsigned:12;
+};
+
+struct sniffer_buf{
+  struct RxControl rx_ctrl;
+  u8 buf[48];// header contains ieee80211
+  u16 cnt;// Number of packages
+};
+
 void callback(uint8 *buf, uint16 len) {
-  Serial.println("callback called");
-  Serial.println(len);
+    uint16 i;
+    uint8_t type;
+
+    lpframectrl_80211 framectrl;
+    struct router_info *info = NULL;
+    if (len < sizeof(struct RxControl) + sizeof(struct w80211_header)) {
+        return;
+    }
+    struct sniffer_buf *sniffer = (struct sniffer_buf*)buf;
+    buf +=sizeof(struct RxControl);
+    struct w80211_header *probe_buf = (struct w80211_header*)buf;
+    Serial.print("r ");
+    for(int i=0; i<6; i++) {
+      Serial.print(probe_buf->rdaddr[i], HEX);
+      Serial.print(':');
+    }
+    Serial.println();
+
+    Serial.print("t ");
+    for(int i=0; i<6; i++) {
+      Serial.print(probe_buf->tsaddr[i], HEX);
+      Serial.print(':');
+    }
+    Serial.println();
+
+    Serial.print("b ");
+    for(int i=0; i<6; i++) {
+      Serial.print(probe_buf->bssid[i], HEX);
+      Serial.print(':');
+    }
+    Serial.println("\n---------");
 }
 
 void setup()
